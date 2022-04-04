71235383378487ac43d8871ccc903c53bbb3e0df
mfort-youngcheol <yc.son@mfort.co.kr>
2022-04-04 16:32:21 +0900

    add environment variable validation

--- src/main/kotlin/com/example/batch/SpringBatchTestApplication.kt
+++ src/main/kotlin/com/example/batch/SpringBatchTestApplication.kt
@@ -1,12 +1,24 @@
 package com.example.batch
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import javax.annotation.PostConstruct
 
 @EnableBatchProcessing
 @SpringBootApplication
-class SpringBatchTestApplication
+class SpringBatchTestApplication {
+    @Value("\${spring.batch.job.names:}")
+    private lateinit var jobNames: String
+
+    @PostConstruct
+    fun checkBatchEnvironmentVariable() {
+        require(jobNames.isNotBlank()) {
+            "`spring.batch.job.names` value must not be empty"
+        }
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<SpringBatchTestApplication>(*args)
